01b43dec971c3b59ed57414e0560a16632104ec9
nhardwajvivekkumar <vk3411389@gmail.com>
2025-07-10 16:30:42 +0530

    Updated controller

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { UserLogin, UserRegister } from "../controllers/UserController.js";
+import {
+  UserLogin,
+  UserRegister,
+  addWorkout,
+  getUserDashboard,
+  getWorkoutsByDate,
+} from "../controllers/UserController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/signup", UserRegister);
 router.post("/login", UserLogin);
+
+router.get("/dashboard", verifyToken, getUserDashboard);
+router.get("/workout", verifyToken, getWorkoutsByDate);
+router.post("/workout", verifyToken, addWorkout);
 
 export default router;
